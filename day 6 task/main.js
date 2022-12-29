93bf71587d53d7a4def48c70fb8b3d0fc4bcbdd4
memmedli elvin <memmedlielvindevop@gmail.com>
2022-12-29 21:29:13 +0400

    test-2

---
+++ day 6 task/main.js
@@ -0,0 +1,47 @@
+function show1() {
+  document.querySelector(".showImgHere").src = "clock-small.png";
+}
+
+function show2() {
+  document.querySelector(".showImgHere").src = "img1.png";
+}
+
+function show3() {
+  document.querySelector(".showImgHere").src = "img2.png";
+}
+
+function show4() {
+  document.querySelector(".showImgHere2").src = "clock-small.png";
+}
+
+function show5() {
+  document.querySelector(".showImgHere2").src = "img1.png";
+}
+
+function show6() {
+  document.querySelector(".showImgHere2").src = "img2.png";
+}
+
+function show7() {
+  document.querySelector(".showImgHere3").src = "clock-small.png";
+}
+
+function show8() {
+  document.querySelector(".showImgHere3").src = "img1.png";
+}
+
+function show9() {
+  document.querySelector(".showImgHere3").src = "img2.png";
+}
+
+function show10() {
+  document.querySelector(".showImgHere4").src = "clock-small.png";
+}
+
+function show11() {
+  document.querySelector(".showImgHere4").src = "img1.png";
+}
+
+function show12() {
+  document.querySelector(".showImgHere4").src = "img2.png";
+}
